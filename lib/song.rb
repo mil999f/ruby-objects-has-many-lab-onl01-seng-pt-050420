cfd2dc3d598dd6eeb6d49b6d4be8aab79ee876b1
Milly Flores <miflo0h@gmail.com>
2020-06-22 06:56:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,22 @@
-class Song
-  attr_accessor :artist, :name
-
+class Song 
+  attr_accessor :name, :artist
+  
+  @@all = [] 
+  
   def initialize(name)
     @name = name
+    save
   end
-
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
   def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
+    artist.name if artist
   end
-
 end
