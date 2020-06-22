e97c19e2458ed63f0b82722fa6772dfb04c35f15
Milly Flores <miflo0h@gmail.com>
2020-06-22 06:46:39 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,31 @@
-class Author
+class Author 
+  attr_accessor :name
+  @@post_count = 0
 
-    attr_accessor :name, :posts
-  
-    @@post_count = 0
-    
-    def initialize(name)
-      @name = name
-      @posts = []
-    end
-  
-    def add_post(post)
-      @posts << post
-      post.author = self
-      @@post_count += 1
-    end
-  
-    def add_post_by_title(title)
-      post = Post.new(title)
-      @posts << post
-      post.author = self
-      @@post_count += 1
-    end
-  
-    def self.post_count
-      @@post_count
-    end
+  def initialize(name)
+    @name = name
+    @posts = []
   end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end 
