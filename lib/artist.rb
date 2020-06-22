ce7b4d071eb28e7d7b17bfbea7deda6d803a12af
Milly Flores <miflo0h@gmail.com>
2020-06-22 06:54:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,25 @@
-class Artist
+class Artist 
   attr_accessor :name
-  @@song_count = 0
+  
   def initialize(name)
     @name = name
-    @songs = [] #empty collection of songs
+    @songs = []
   end
-
-  def songs
-    @songs
-  end
-
+  
   def add_song(song)
-    @songs << song
     song.artist = self
-    @@song_count += 1
   end
-
+  
+  def songs 
+    Song.all
+  end
+  
   def add_song_by_name(song_name)
     song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    add_song(song)
   end
 
   def self.song_count
-    @@song_count
+    Song.all.size
   end
-
-end 
- 31  lib/author.rb 
+end
